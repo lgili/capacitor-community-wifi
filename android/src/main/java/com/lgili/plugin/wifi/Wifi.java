c2a97d25239f199725afc730c0a9995d47215d58
lgili <luiz.gili@posgrad.ufsc.br>
2023-05-18 16:19:07 -0300

    Initial commit

---
+++ android/src/main/java/com/lgili/plugin/wifi/Wifi.java
@@ -0,0 +1,11 @@
+package com.lgili.plugin.wifi;
+
+import android.util.Log;
+
+public class Wifi {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
---
+++ android/src/main/java/com/lgili/plugin/wifi/WifiPlugin.java
@@ -0,0 +1,22 @@
+package com.lgili.plugin.wifi;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "Wifi")
+public class WifiPlugin extends Plugin {
+
+    private Wifi implementation = new Wifi();
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
+    }
+}
